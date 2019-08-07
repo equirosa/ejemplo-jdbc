e39e9b8fb668239c216b9cb68d48e42870fa1708
Eduardo Quiros <equirosa@ucenfotec.ac.cr>
2019-08-07 14:24:11 -0600

    Cambiar indentacion.

--- src/JDBCExampleMySql.java
+++ src/JDBCExampleMySql.java
@@ -6,161 +6,161 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class JDBCExampleMySql {
-
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static PrintStream out = System.out;
-
-    public static void main (String args[]) throws java.io.IOException {
-        try {
-            mostrarMenu();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    public static void mostrarMenu() throws IOException{
-        int opcion;
-        opcion = -1;
-        do{
-            out.println("1.Listar Personas");
-            out.println("2.Registrar Persona");
-            out.println("3.Modificar Persona ");
-            out.println("4. Eliminar Persona ");
-            out.println("0.Salir");
-            out.println("Digite la opción que dese");
-            opcion = Integer.parseInt(in.readLine());
-            procesarOpcion(opcion);
-        }while(opcion != 0);
-    }
-    //Rutina para procesar la opción seleccionada por el usuario
-    public static void procesarOpcion(int pOpcion)
-            throws java.io.IOException{
-        switch(pOpcion){
-            case 1:
-                listar();
-                break;
-            case 2:
-                registrar();
-                break;
-            case 3:
-                modificar();
-                break;
-            case 4:
-                eliminar();
-                break;
-            case 0:
-                out.println("Adiós");
-                break;
-            default:
-                out.println("Opción inválida");
-        }
-    }
-    public static void listar()
-    {
-        try {
-            // The newInstance() call is a work around for some
-            // broken Java implementations
-
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            Connection conn = null;
-            Statement stmt = null;
-            ResultSet rs = null;
-            ArrayList<Persona> lista = new ArrayList<>();
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/Universidad?" +
-                    "user=root&password=");
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT * FROM Persona");
-            while(rs.next())
-            {
-                Persona p = new Persona();
-                p.setNombre(rs.getString("nombre"));
-                p.setCedula(rs.getString("cedula"));
-                p.setApellido(rs.getString("apellido"));
-                lista.add(p);
-            }
-
-            //se imprime el Array list
-            for(Persona p : lista){
-                out.println(p.toString());
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    public  static void registrar()
-    {
-        try {
-            // The newInstance() call is a work around for some
-            // broken Java implementations
-
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            Connection conn = null;
-            Statement stmt = null;
-            String[] info = pedirInfoPersona();
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/Universidad?" +
-                    "user=root&password=");
-            stmt = conn.createStatement();
-            stmt.execute("INSERT INTO Persona(NOMBRE,APELLIDO,CEDULA) VALUES " +
-                    "('"+ info[0] + "','" + info[1] + "','" +
-                    info[2] + "')");
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    public  static void modificar()
-    {
-        try {
-            // The newInstance() call is a work around for someClass.forName("com.mysql.jdbc.Driver").newInstance();
-            // broken Java implementations
-
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            Connection conn = null;
-            Statement stmt = null;
-            String[] info = pedirInfoPersona();
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/Universidad?" +
-                    "user=root&password=");
-
-            stmt = conn.createStatement();
-            String cmdText = "UPDATE Persona SET nombre = '" +
-                    info[0] + "',apellido = '" + info[1] + "'" +
-                    " WHERE cedula = '" + info[2] + "'";
-            stmt.execute(cmdText);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-    
-    private static String[] pedirInfoPersona() throws IOException {
-        String[] info = new String[3];
-        out.println("Introduzca el nombre de la persona.");
-        info[0]=in.readLine();
-        out.println("Introduzca el apellido de la persona.");
-        info[1]=in.readLine();
-        out.println("Introduzca la cedula de la persona.");
-        info[2]=in.readLine();
-        return info;
-    }
-    
-    public  static void eliminar()
-    {
-        try {
-            // The newInstance() call is a work around for some
-            // broken Java implementations
-
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            Connection conn = null;
-            Statement stmt = null;
-            out.println("introduzca la cedula del usuario que desea eliminar.");
-            String cedula = in.readLine();
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/Universidad?" +
-                    "user=root&password=");
-            stmt = conn.createStatement();
-            stmt.execute("DELETE  FROM Persona  WHERE cedula = '" + cedula + "'");
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+	
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static PrintStream out = System.out;
+	
+	public static void main (String args[]) throws java.io.IOException {
+		try {
+			mostrarMenu();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public static void mostrarMenu() throws IOException{
+		int opcion;
+		opcion = -1;
+		do{
+			out.println("1.Listar Personas");
+			out.println("2.Registrar Persona");
+			out.println("3.Modificar Persona ");
+			out.println("4. Eliminar Persona ");
+			out.println("0.Salir");
+			out.println("Digite la opción que dese");
+			opcion = Integer.parseInt(in.readLine());
+			procesarOpcion(opcion);
+		}while(opcion != 0);
+	}
+	//Rutina para procesar la opción seleccionada por el usuario
+	public static void procesarOpcion(int pOpcion)
+			throws java.io.IOException{
+		switch(pOpcion){
+			case 1:
+				listar();
+				break;
+			case 2:
+				registrar();
+				break;
+			case 3:
+				modificar();
+				break;
+			case 4:
+				eliminar();
+				break;
+			case 0:
+				out.println("Adiós");
+				break;
+			default:
+				out.println("Opción inválida");
+		}
+	}
+	public static void listar()
+	{
+		try {
+			// The newInstance() call is a work around for some
+			// broken Java implementations
+			
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			Connection conn = null;
+			Statement stmt = null;
+			ResultSet rs = null;
+			ArrayList<Persona> lista = new ArrayList<>();
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/Universidad?" +
+					"user=root&password=");
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM Persona");
+			while(rs.next())
+			{
+				Persona p = new Persona();
+				p.setNombre(rs.getString("nombre"));
+				p.setCedula(rs.getString("cedula"));
+				p.setApellido(rs.getString("apellido"));
+				lista.add(p);
+			}
+			
+			//se imprime el Array list
+			for(Persona p : lista){
+				out.println(p.toString());
+			}
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public  static void registrar()
+	{
+		try {
+			// The newInstance() call is a work around for some
+			// broken Java implementations
+			
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			Connection conn = null;
+			Statement stmt = null;
+			String[] info = pedirInfoPersona();
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/Universidad?" +
+					"user=root&password=");
+			stmt = conn.createStatement();
+			stmt.execute("INSERT INTO Persona(NOMBRE,APELLIDO,CEDULA) VALUES " +
+					"('"+ info[0] + "','" + info[1] + "','" +
+					info[2] + "')");
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public  static void modificar()
+	{
+		try {
+			// The newInstance() call is a work around for someClass.forName("com.mysql.jdbc.Driver").newInstance();
+			// broken Java implementations
+			
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			Connection conn = null;
+			Statement stmt = null;
+			String[] info = pedirInfoPersona();
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/Universidad?" +
+					"user=root&password=");
+			
+			stmt = conn.createStatement();
+			String cmdText = "UPDATE Persona SET nombre = '" +
+					info[0] + "',apellido = '" + info[1] + "'" +
+					" WHERE cedula = '" + info[2] + "'";
+			stmt.execute(cmdText);
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	private static String[] pedirInfoPersona() throws IOException {
+		String[] info = new String[3];
+		out.println("Introduzca el nombre de la persona.");
+		info[0]=in.readLine();
+		out.println("Introduzca el apellido de la persona.");
+		info[1]=in.readLine();
+		out.println("Introduzca la cedula de la persona.");
+		info[2]=in.readLine();
+		return info;
+	}
+	
+	public  static void eliminar()
+	{
+		try {
+			// The newInstance() call is a work around for some
+			// broken Java implementations
+			
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			Connection conn = null;
+			Statement stmt = null;
+			out.println("introduzca la cedula del usuario que desea eliminar.");
+			String cedula = in.readLine();
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/Universidad?" +
+					"user=root&password=");
+			stmt = conn.createStatement();
+			stmt.execute("DELETE  FROM Persona  WHERE cedula = '" + cedula + "'");
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 }
 
