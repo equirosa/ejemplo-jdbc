c1accfb2e023ba0785ce61759c30c2c18ce8bf41
Eduardo Quiros <equirosa@ucenfotec.ac.cr>
2019-08-07 14:47:51 -0600

    Agregar contenido a clase Mascota.

--- src/Mascota.java
+++ src/Mascota.java
@@ -1,2 +1,72 @@
+import java.time.LocalDate;
+
 public class Mascota {
+	private String nombre, raza;
+	private LocalDate fechaNac;
+	private Persona duenno;
+	private int id;
+	
+	public Mascota(String nombre, String raza, LocalDate fechaNac, Persona duenno) {
+		this.nombre = nombre;
+		this.raza = raza;
+		this.fechaNac = fechaNac;
+		this.duenno = duenno;
+	}
+	
+	public Mascota() {
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getRaza() {
+		return raza;
+	}
+	
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	
+	public LocalDate getFechaNac() {
+		return fechaNac;
+	}
+	
+	public void setFechaNac(LocalDate fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+	
+	public Persona getDuenno() {
+		return duenno;
+	}
+	
+	public void setDuenno(Persona duenno) {
+		this.duenno = duenno;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		
+		Mascota mascota = (Mascota) o;
+		
+		return id == mascota.id;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "Mascota{" +
+				"nombre='" + nombre + '\'' +
+				", raza='" + raza + '\'' +
+				", fechaNac=" + fechaNac +
+				", duenno=" + duenno +
+				", id=" + id +
+				'}';
+	}
 }
